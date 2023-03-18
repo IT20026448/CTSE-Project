9f47de2166b2eb2bbb945f0443b097981d70c36a
Oshan Udumulla <oshan.udumullaarachchi@directfn.com>
2023-03-19 01:46:57 +0530

    add login page and test home page

--- WareHouseManagement/firebase-config.js
+++ WareHouseManagement/firebase-config.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth'
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,15 +13,14 @@ export const firebaseConfig = {
   projectId: "warehousemanagement-3136b",
   storageBucket: "warehousemanagement-3136b.appspot.com",
   messagingSenderId: "607915091182",
-  appId: "1:607915091182:web:40a36cda35c2c804f67fff"
+  appId: "1:607915091182:web:40a36cda35c2c804f67fff",
 };
 
 // Initialize Firebase
 //const app = initializeApp(firebaseConfig);
 
-
-if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig)
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-export {firebase};
+export { firebase };
