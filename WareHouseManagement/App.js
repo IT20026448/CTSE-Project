0d480a0bdc6d728abbbcbba48b5e6a654159e998
vibhujay99 <vibhujay99@gmail.com>
2023-03-23 00:36:00 +0530

    Ref() : Home screen and task screen

--- WareHouseManagement/App.js
+++ WareHouseManagement/App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginPage from './components/Loginpage';
-import Home from './components/Home';
+import LoginPage from "./components/Loginpage";
+import Home from "./components/Home";
+import Tasks from "./screens/Task.js";
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="LoginPage">
-      <Stack.Screen name="LoginPage" component={LoginPage} />
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginPage">
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Tasks" component={Tasks} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
--- WareHouseManagement/components/Home.js
+++ WareHouseManagement/components/Home.js
@@ -1,11 +1,206 @@
-import { View,Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import profile from "../assets/tiffany.jpeg";
+import { SafeAreaView } from "react-native-safe-area-context";
+import colors from "../assets/colors";
+import Feather from "react-native-vector-icons/Feather";
+import Entypo from "react-native-vector-icons/Entypo";
+import work from "../assets/work.png";
+import employee from "../assets/employee.jpg";
+import product from "../assets/product.jpg";
 
-
-export default function Home(){
-
-    return(
-        <View>
-        <Text>This is home</Text>
-        </View>
-    )
+export default function Home() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <SafeAreaView>
+          <View style={styles.menuWrapper}>
+            <Feather
+              name="menu"
+              size={32}
+              color={colors.black}
+              style={styles.menuIcon}
+            />
+            <Image source={profile} style={styles.profileImage} />
+          </View>
+        </SafeAreaView>
+        <TouchableOpacity onPress={() => navigation.navigate("Tasks")}>
+          <ImageBackground
+            source={work}
+            style={[styles.discoverItem]}
+            imageStyle={styles.discoverItemImage}
+          >
+            <View style={styles.discoverItemLocationWrapper}>
+              <Entypo name="location-pin" size={18} color={colors.white} />
+              <Text style={styles.discoverItemLocationText}>
+                {"Manage Tasks"}
+              </Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground
+            source={employee}
+            style={[styles.discoverItem]}
+            imageStyle={styles.discoverItemImage}
+          >
+            <View style={styles.discoverItemLocationWrapper}>
+              <Entypo name="location-pin" size={18} color={colors.white} />
+              <Text style={styles.discoverItemLocationText}>
+                {"Manage Employees"}
+              </Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <ImageBackground
+            source={product}
+            style={[styles.discoverItem]}
+            imageStyle={styles.discoverItemImage}
+          >
+            <View style={styles.discoverItemLocationWrapper}>
+              <Entypo name="location-pin" size={18} color={colors.white} />
+              <Text style={styles.discoverItemLocationText}>
+                {"Manage Products"}
+              </Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: colors.white,
+  },
+  menuWrapper: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 52,
+    height: 52,
+    borderRadius: 10,
+  },
+  discoverWrapper: {
+    // marginHorizontal: 20,
+    marginTop: 20,
+  },
+  discoverTitle: {
+    marginHorizontal: 20,
+    fontFamily: "Lato-Bold",
+    fontSize: 32,
+  },
+  discoverCategoriesWrapper: {
+    marginHorizontal: 20,
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  discoverCategoryText: {
+    marginRight: 30,
+    fontFamily: "Lato-Regular",
+    fontSize: 16,
+    color: colors.gray,
+  },
+  discoverItemsWrapper: {
+    paddingVertical: 20,
+  },
+  discoverItem: {
+    width: 390,
+    height: 250,
+    justifyContent: "flex-end",
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    marginRight: 20,
+    marginLeft: 10,
+    marginTop: 30,
+  },
+  discoverItemImage: {
+    borderRadius: 20,
+  },
+  discoverItemTitle: {
+    fontFamily: "Lato-Bold",
+    fontSize: 18,
+    color: colors.white,
+  },
+  discoverItemLocationWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  discoverItemLocationText: {
+    marginLeft: 5,
+    fontFamily: "Lato-Bold",
+    fontSize: 14,
+    color: colors.white,
+  },
+  activitiesWrapper: {
+    marginTop: 10,
+  },
+  activitiesTitle: {
+    marginHorizontal: 20,
+    fontFamily: "Lato-Bold",
+    fontSize: 24,
+    color: colors.black,
+  },
+  activitiesItemsWrapper: {
+    paddingVertical: 20,
+  },
+  activityItemWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  activityItemImage: {
+    width: 36,
+  },
+  activityItemText: {
+    marginTop: 5,
+    fontFamily: "Lato-Bold",
+    fontSize: 14,
+    color: colors.gray,
+  },
+  learnMoreWrapper: {
+    marginTop: 10,
+  },
+  learnMoreTitle: {
+    marginHorizontal: 20,
+    fontFamily: "Lato-Bold",
+    fontSize: 24,
+    color: colors.black,
+  },
+  learnMoreItemsWrapper: {
+    paddingVertical: 20,
+  },
+  learnMoreItem: {
+    width: 170,
+    height: 180,
+    justifyContent: "flex-end",
+    marginRight: 20,
+  },
+  learnMoreItemImage: {
+    borderRadius: 20,
+  },
+  learnMoreItemText: {
+    fontFamily: "Lato-Bold",
+    fontSize: 18,
+    color: colors.white,
+    marginHorizontal: 10,
+    marginVertical: 20,
+  },
+});
