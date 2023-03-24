3093a0d9e6170301299fbe7c60d30a907b505b58
Oshan Udumulla <oshan.udumullaarachchi@directfn.com>
2023-03-24 23:07:52 +0530

    relocation and updates

--- WareHouseManagement/App.js
+++ WareHouseManagement/App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginPage from './components/Loginpage';
-import Home from './components/Home';
+import LoginPage from "./components/IT20090944/LoginRegister/LoginRegisterpage";
+import Home from "./components/Home";
+import AllItems from "./components/IT20090944/ItemsManage/AllItems";
+import AddUpdateProduct from "./components/IT20090944/ItemsManage/AddUpdateProduct";
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="LoginPage">
-      <Stack.Screen name="LoginPage" component={LoginPage} />
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="Loginpage">
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AllItems" component={AllItems} />
+        <Stack.Screen
+          name="Add"
+          component={AddUpdateProduct}
+          options={{ title: "Add Product" }}
+          initialParams={{ item: null }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-
+//
